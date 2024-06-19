93d2d24d7a11c0662c71f68af4b6f838f3a99c47
JoJobani <hvu2309@gmail.com>
2024-06-19 16:35:30 +0300

    Fix map bug

--- js/services/map.service.js
+++ js/services/map.service.js
@@ -23,7 +23,7 @@ function initMap(lat = 32.0749831, lng = 34.9120554) {
         })
 }
 
-function panTo({lat, lng, zoom=15}) {
+function panTo({ lat, lng, zoom = 15 }) {
     const laLatLng = new google.maps.LatLng(lat, lng)
     gMap.panTo(laLatLng)
     gMap.setZoom(zoom)
@@ -34,9 +34,10 @@ function lookupAddressGeo(geoOrAddress) {
     // const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}`
     // const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452`
 
-    var url = `https://maps.googleapis.com/maps/api/geocode/json?key=${API_KEY}&`
+    var url = `https://maps.googleapis.com/maps/api/geocode/json?`
     url += (geoOrAddress.lat) ? `latlng=${geoOrAddress.lat},${geoOrAddress.lng}` :
         `address=${geoOrAddress}`
+    url += `&key=${API_KEY}`
 
     return fetch(url)
         .then(res => res.json())
@@ -55,7 +56,6 @@ function lookupAddressGeo(geoOrAddress) {
             // console.log('GEO IS', geo)
             return geo
         })
-
 }
 
 function addClickListener(cb) {
